d2fc587b595e7e4785ceea2a8405b0249c335b36
Zakaria77mekki <zakaria-abdenour.mekki@ensia.edu.dz>
2025-08-25 21:15:48 +0100

    5th portfolio commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '..'))); // Serve files from parent directory
+app.use(express.static('.')); // Serve files from current directory
 
 // Redirect root to landing page
 app.get('/', (req, res) => {
